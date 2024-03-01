12349a17ec753e1773cdab880d6a211d72fd3ee9
Edwin_liu1 <Edwin.liu25@ycdsbk12.ca>
2024-03-01 13:09:36 -0500

    end of class

--- Sketch.java
+++ Sketch.java
@@ -6,9 +6,23 @@ public class Sketch extends PApplet {
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
+
+   int intScreenSize;
+   int intPosX;
+   int intPosY;
+   int intMoveX;
+   int intMoveY;
+
   public void settings() {
 	// put your size call here
     size(400, 400);
+
+    intPosX = width / 2;
+    intPosY = height / 2;
+
+    intMoveX = (int)random(-5, 5);
+    intMoveY = (int)random(-5, 5);
+
   }
 
   /** 
@@ -16,21 +30,25 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(255, 255, 255);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  public void draw() {
+    background(255);
+    fill(0);
+	  ellipse( intPosX, intPosY, 10, 10);
+
+    intPosX = intPosX + intMoveX;
+    intPosY = intPosY + intMoveY;
+
+    if (intPosX - 10 <= 0) {
+      intMoveX = random(1, 5);
+    }
+
   }
-  
-  // define other methods down here.
+
 }
